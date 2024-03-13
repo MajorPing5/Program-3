c13abb2e2344efd109f9c27d2030dc05e7bc3e3b
Robert-Abel Faustino Porras <majorping5@gmail.com>
2024-03-13 17:55:47 -0500

    Begins introduction to parsing string by whitespace
    This is the beginning of me trying to parse each entry by its whitespace, specific to the TRANSACTION text file for now. This entry of code is not complete, and as such is still riddled with bugs galore.

--- Program 3.rkt
+++ Program 3.rkt
@@ -19,6 +19,7 @@ be created. Enable/disable using either #t or #f appropriately |#
     (let loop()
       (let ([data (read-line in-port)])
         (unless (eof-object? data)
+          (map (λ (line) (string-split line #\space)) lines)
           (displayln data)
           (loop))))
     (printf "\nEnd of file\n\n")))
@@ -36,3 +37,14 @@ be created. Enable/disable using either #t or #f appropriately |#
   )
 
 (main)|#
+
+#|
+;; Read lines from a file (replace "your-file.txt" with the actual file path)
+(define lines (read-line "your-file.txt"))
+
+;; Split each line into words
+(define words-list (map (λ (line) (string-split line #\space)) lines))
+
+;; Print the words for each line
+(for-each (λ (words) (displayln words)) words-list)
+|#
