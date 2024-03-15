8298448512f058cae2be2ec8343f5f3f5f442693
Robert-Abel Faustino Porras <majorping5@gmail.com>
2024-03-15 12:01:37 -0500

    Condenses Read Functions into 1 & Finishes Parsing by Whitespace

--- Program 3.rkt
+++ Program 3.rkt
@@ -1,50 +1,41 @@
-#lang racket/gui
+#lang racket
 
 #| The following under this line will dictate if an ouput is or is not to
 be created. Enable/disable using either #t or #f appropriately |#
 (define create_output #f)
 
-;; The following 3 functions manage the input/output of the files for this programming assignment.
-(call-with-input-file "ACCOUNTS.TXT"
-  (λ (in-port)
-    (let loop()
-      (let ([data (read-line in-port)])
-        (unless (eof-object? data)
-          (displayln data)
-          (loop))))
-    (printf "End of file\n\n")))
+;; The following 3 functions manage the input of the files for this assignment.
 
-(call-with-input-file "TRANSACTIONS.TXT"
-  (λ (in-port)
-    (let loop()
-      (let ([data (read-line in-port)])
-        (unless (eof-object? data)
-          (map (λ (line) (string-split line #\space)) lines)
-          (displayln data)
-          (loop))))
-    (printf "\nEnd of file\n\n")))
+(define (process-account-line line)
+  (regexp-split #px"  +" line)) ;; Separates by 2 spaces in accounts file
 
-(if create_output
-  [call-with-output-file "STATEMENT.TXT"
-    (λ (out-port)
-      [display "Output functions as intended." out-port])]
-  (begin
-         (displayln "Output file has not been created due to initial Bool call.")
-         (displayln "To create the output file, change line 5 to '#t' and try again")))
+(define (process-transaction-line line)
+  (string-split line)) ;; Separates by tab character in accounts file
 
-#|(define main
-;; To Be Implemented
-  )
+(define (process-file filename process-line)
+  (call-with-input-file filename
+    (λ (in-port)
+      (let loop ([line (read-line in-port)])
+        (if (eof-object? line)
+            (printf "\nEnd of file\n\n") ;; If the line is an end of line object
+            (let ([data (process-line line)]) ;; If the line contains data
+              (displayln data)
+              (loop (read-line in-port))))))))
 
-(main)|#
 
-#|
-;; Read lines from a file (replace "your-file.txt" with the actual file path)
-(define lines (read-line "your-file.txt"))
+;; This function is intended to execute if and only if line 5 is set to true
+(define (create_output_check)
+  (if create_output
+      [call-with-output-file "STATEMENT.TXT"
+        (λ (out-port)
+          [display "Output functions as intended." out-port])]
+      (begin
+        (displayln "Output file has not been created due to initial Bool call.")
+        (displayln "To create the output file, change line 5 to '#t' and try again"))))
 
-;; Split each line into words
-(define words-list (map (λ (line) (string-split line #\space)) lines))
+(define (main)
+  (process-file "ACCOUNTS.TXT" process-account-line)
+  (process-file "TRANSACTIONS.TXT" process-transaction-line)
+  (create_output_check))
 
-;; Print the words for each line
-(for-each (λ (words) (displayln words)) words-list)
-|#
+(main)
