e5614df54505bf804633b6188e42e39c73c75ff3
Robert-Abel Faustino Porras <majorping5@gmail.com>
2024-03-15 13:45:23 -0500

    Begins work on proper formatting of data
    As it currently stands, the data for "ACCOUNTS.TXT" is being read into its data form incorrectly. Using Copilot, I was getting the data to have more spaces and even including the newline character at the end of each entry, thus completely messing up its proper format reading. However, using the same line of code I found that "TRANSACTIONS.TXT" had no such problems and was read in appropriately. Neither of which in the data stream are being read in as a whole list however, which might be a problem worth investigating to debug this section of code prior to data manipulation. I have swapped between Copilot and ChatGPT for different ideas, and am seeing if using both might lead to better results.

--- Program 3.rkt
+++ Program 3.rkt
@@ -1,24 +1,19 @@
 #lang racket
 
-#| The following under this line will dictate if an ouput is or is not to
+#| The following under this line will dictate if the ouput file is or is not to
 be created. Enable/disable using either #t or #f appropriately |#
 (define create_output #f)
 
 ;; The following 3 functions manage the input of the files for this assignment.
 
-(define (process-account-line line)
-  (regexp-split #px"  +" line)) ;; Separates by 2 spaces in accounts file
-
-(define (process-transaction-line line)
-  (string-split line)) ;; Separates by tab character in accounts file
-
-(define (process-file filename process-line)
+(define (process-file filename)
   (call-with-input-file filename
     (λ (in-port)
       (let loop ([line (read-line in-port)])
         (if (eof-object? line)
             (printf "\nEnd of file\n\n") ;; If the line is an end of line object
-            (let ([data (process-line line)]) ;; If the line contains data
+            (let* ([normalized-line (regexp-replace* #rx"\r|\n" (string-trim line) "")]
+                   [data (string-split normalized-line #rx"(/s/s+)|[\t]+")]) ;; Look for double space or tab special character
               (displayln data)
               (loop (read-line in-port))))))))
 
@@ -27,15 +22,15 @@ be created. Enable/disable using either #t or #f appropriately |#
 (define (create_output_check)
   (if create_output
       [call-with-output-file "STATEMENT.TXT"
-        (λ (out-port)
+        (λ (out-port) ;; If Line 5 is True
           [display "Output functions as intended." out-port])]
-      (begin
+      (begin ;; If Line 5 is False
         (displayln "Output file has not been created due to initial Bool call.")
         (displayln "To create the output file, change line 5 to '#t' and try again"))))
 
 (define (main)
-  (process-file "ACCOUNTS.TXT" process-account-line)
-  (process-file "TRANSACTIONS.TXT" process-transaction-line)
+  (process-file "ACCOUNTS.TXT")
+  (process-file "TRANSACTIONS.TXT")
   (create_output_check))
 
 (main)
